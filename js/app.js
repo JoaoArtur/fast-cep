5a4cdf003e6272b7f9f23a3f44167e1981cc20d7
João Artur <joao09092002@gmail.com>
2017-01-10 10:10:07 -0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 $(function() {
-  
+  $('.formulario').submit(function() {
+    var cep = $('input[name="cep"]').val();{
+    $('.resultado').empty().append('<span class="fa fa-spinner load"></span>');
+    if (!empty(cep)) {
+      $.get("https://viacep.com.br/ws/"+cep+"/json/unicode/",function(resultado) {
+        $('.fa.fa-spinner.load').fadeOut().hide();
+        console.log(cep);
+      });
+    } else {
+      $('.resultado').empty().append('CEP Inválido');
+    }
+    return false;
+  });
 });
