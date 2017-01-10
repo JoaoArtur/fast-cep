c8fe580e5f22888f82d9745427d41bacfcf39b84
João Artur <joao09092002@gmail.com>
2017-01-10 10:32:36 -0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,8 +6,38 @@ $(function() {
       $.get("https://viacep.com.br/ws/"+cep+"/json/unicode/",function(resultado) {
         $('.fa.fa-spinner.load').fadeOut().hide();
         console.log(resultado);
-        $('.resultado').empty().append("Rua "+resultado.logradouro+"<br>"+"Bairro "+resultado.bairro+"<br>"+resultado.cidade+" - "+resultado.uf);
-      });
+        $('.resultado').empty().append("<div id='r1'><div><div id='r2'><div><div id='r3'><div>");
+        $("#r1").typed({
+            strings: ["<b>Rua</b> "+resultado.logradouro],
+            typeSpeed: 30,
+            backDelay: 500,
+            loop: false,
+            contentType: 'html',
+            loopCount: false,
+            callback: function(){ foo(); },
+            resetCallback: function() { newTyped(); }
+        });
+        $("#r2").typed({
+            strings: ["<b>Bairro</b> "+resultado.bairro],
+            typeSpeed: 30,
+            backDelay: 500,
+            loop: false,
+            contentType: 'html',
+            loopCount: false,
+            callback: function(){ foo(); },
+            resetCallback: function() { newTyped(); }
+        });
+        $("#r3").typed({
+            strings: [resultado.cidade+" - "+resultado.uf],
+            typeSpeed: 30,
+            backDelay: 500,
+            loop: false,
+            contentType: 'html',
+            loopCount: false,
+            callback: function(){ foo(); },
+            resetCallback: function() { newTyped(); }
+        });
+       });
     } else {
       $('.resultado').empty().append('CEP Inválido');
     }
